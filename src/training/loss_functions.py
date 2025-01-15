79ecbeccda6c114f8fcca1f6db982c18040f6fd4
Inês Cunha <inescunha200@gmail.com>
2025-01-15 17:15:35 +0100

    used loss functions for now

---
+++ src/training/loss_functions.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+
+def mse_loss(y_true, y_pred):
+    return tf.reduce_mean(tf.square(y_true - y_pred))
+
+##### BCE and CCE Losses #####
+bce = tf.keras.losses.BinaryCrossentropy(from_logits=False, label_smoothing=0.001)
+cce = tf.keras.losses.CategoricalCrossentropy(from_logits=False)
+
+def z_mean_var_loss(z):
+    loss = tf.abs(tf.math.reduce_variance(z) - 1) + tf.abs(tf.math.reduce_mean(z))
+    return tf.reduce_mean(loss)
+
+##### COVARIANCE LOSS #####
+def get_off_diag_values(x):
+    x_flat = tf.reshape(x,[-1])[:-1]
+    x_reshape = tf.reshape(x_flat,[x.shape[0]-1, x.shape[0]+1])[:, 1:]
+    off_diag_values = tf.reshape(x_reshape,[-1])
+    return off_diag_values
+
+def cov_loss_terms(z_batch):
+    z_batch = z_batch - tf.reduce_mean(z_batch, axis=0)
+    z_std = tf.sqrt(tf.math.reduce_variance(z_batch, axis=0) + 0.0001)
+    z_std_loss = tf.reduce_mean( tf.nn.relu(1 - z_std) )
+    cov = (tf.transpose(z_batch) @ z_batch) / (z_batch.shape[0] ) 
+    diag_cov = tf.linalg.diag_part(cov) 
+    diag_cov_mean = tf.reduce_mean(tf.abs(diag_cov))    
+    off_diag_loss = tf.reduce_mean(tf.abs(get_off_diag_values(cov)))
+    return cov, z_std_loss, diag_cov_mean , off_diag_loss
+
