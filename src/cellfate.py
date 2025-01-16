d207e171ad807cf909713a1c09f70b9b8dd7f83c
Inês Cunha <inescunha200@gmail.com>
2025-01-16 16:27:37 +0100

    starting a training workflow in cellfate.py

---
+++ src/cellfate.py
@@ -0,0 +1,27 @@
+import numpy as np
+import os
+import tensorflow as tf
+from src.training.train import train_model 
+
+# Main function for training
+def main():
+    # Load the data
+    x_train = np.load('./data/stretched_x_train.npy') #TODO later: replace with data loader
+
+    # Config for training
+    config = {
+        'batch_size': 30,
+        'epochs': 10,
+        'learning_rate': 0.001,
+        'seed': 42,
+        'latent_dim': 20,
+        'GaussianNoise_std': 0.003,
+        'lambda_recon': 5, 
+        'lambda_adv': 0.05,
+    }
+
+    # Call the train_model function
+    train_model(config, x_train)
+
+if __name__ == '__main__':
+    main()
