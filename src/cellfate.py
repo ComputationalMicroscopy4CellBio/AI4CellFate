83f036f782c07647ed466aafdd9565f35525cd50
Inês Cunha <inescunha200@gmail.com>
2025-01-18 14:13:42 +0100

    cellfate without mlp built in

--- src/cellfate.py
+++ src/cellfate.py
@@ -1,7 +1,7 @@
 import numpy as np
 import os
 import tensorflow as tf
-from src.training.train import train_model, train_cellfate
+from src.training.train import train_model, train_cellfate, train_cov
 from src.evaluation.evaluate import Evaluation
 from src.training.loss_functions import cov_loss_terms
 
@@ -28,9 +28,9 @@ def evaluate_model(encoder, decoder, classifier, x_train, y_train, x_test, y_tes
     
     if full_evaluation: 
         # Predict labels and plot confusion matrix
-        test_latent_space, _ = encoder.predict(x_test)
-        y_pred = classifier.predict(test_latent_space) 
-        evaluator.plot_confusion_matrix(y_test, y_pred, num_classes=2)
+        # test_latent_space, _ = encoder.predict(x_test)
+        # y_pred = classifier.predict(test_latent_space) 
+        # evaluator.plot_confusion_matrix(y_test, y_pred, num_classes=2)
         
         # Visualize latent space
         evaluator.visualize_latent_space(z_imgs, y_train)
@@ -55,7 +55,7 @@ def main():
         'epochs': 10,
         'learning_rate': 0.001,
         'seed': 69,
-        'latent_dim': 20,
+        'latent_dim': 10,
         'GaussianNoise_std': 0.003,
         'lambda_recon': 5, 
         'lambda_adv': 0.05,
@@ -73,10 +73,10 @@ def main():
 
     config = {
         'batch_size': 30,
-        'epochs': 50,
-        'learning_rate': 0.0001,
+        'epochs': 30,
+        'learning_rate': 0.001,
         'seed': 69,
-        'latent_dim': 20,
+        'latent_dim': 10,
         'GaussianNoise_std': 0.003,
         'lambda_recon': 5, 
         'lambda_adv': 0.05,
@@ -84,14 +84,21 @@ def main():
         'lambda_cov': 0.1,
     }
 
-    # Train the full model
-    full_model_results = train_cellfate(config, encoder, decoder, discriminator, x_train, y_train, x_test, y_test)
+    # Train the model with cov only
+    full_model_results = train_cov(config, encoder, decoder, discriminator, x_train, y_train)
     final_encoder = full_model_results['encoder']
     final_decoder = full_model_results['decoder']
-    classifier = full_model_results['classifier']
+
+    evaluate_model(final_encoder, final_decoder, 0, x_train, y_train, x_test, y_test, full_evaluation=True)
+
+    # Train the full model
+    # full_model_results = train_cellfate(config, encoder, decoder, discriminator, x_train, y_train, x_test, y_test)
+    # final_encoder = full_model_results['encoder']
+    # final_decoder = full_model_results['decoder']
+    # classifier = full_model_results['classifier']
 
     # Evaluate the model
-    evaluate_model(final_encoder, final_decoder, classifier, x_train, y_train, x_test, y_test, full_evaluation=True)
+    #evaluate_model(final_encoder, final_decoder, classifier, x_train, y_train, x_test, y_test, full_evaluation=True)
 
 if __name__ == '__main__':
     main()
