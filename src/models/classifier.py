4303b9d874da69fdf83b011d09031df13c46772a
Inês Cunha <inescunha200@gmail.com>
2025-01-18 13:14:04 +0100

    better classifier

--- src/models/classifier.py
+++ src/models/classifier.py
@@ -1,4 +1,6 @@
 from tensorflow.keras import layers, Sequential
+import tensorflow as tf
+from tensorflow.keras.regularizers import l2
 
 # A small MLP for classification from the latent space
 def mlp_classifier(latent_dim):
@@ -6,6 +8,8 @@ def mlp_classifier(latent_dim):
         layers.Input(shape=(latent_dim,)),
         layers.Dense(32, activation='relu'),
         layers.Dropout(0.3),
-        layers.Dense(16, activation='relu'),
-        layers.Dense(2, activation='softmax')  # Output probabilities for two classes
+        layers.Dense(16, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(1e-4)),
+        layers.Dropout(0.3),
+       # layers.Dense(16, activation='relu'),
+        layers.Dense(2, activation='softmax')
     ])
