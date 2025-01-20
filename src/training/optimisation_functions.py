9610c40b4ae7bbc4ee2b50e0e1a132382b70eb69
Luca Panconi <luca.panconi7@gmail.com>
2025-01-20 14:36:42 +0100

    Delete old scale function

--- src/training/optimisation_functions.py
+++ src/training/optimisation_functions.py
@@ -8,14 +8,3 @@ def L2_norm(grad_list):
     if len(squares) == 0:
         return tf.constant(0.0, dtype=tf.float32)
     return tf.sqrt(tf.add_n(squares))
-
-def scale_gradients(grad_list, norm, lam=1, eps=1e-8):
-    scaled = []
-    for g in grad_list:
-        if g is None:
-            scaled.append(None)
-        else:
-            # Here: g is shape (same as variable shape),
-            # norm is scalar, lam is scalar
-            scaled.append(g * lam / (norm + eps))
-    return scaled
