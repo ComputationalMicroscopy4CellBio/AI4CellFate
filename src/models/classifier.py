907e0292031dc1aa83752c66d9afbc347a787f7c
Inês Cunha <inescunha200@gmail.com>
2025-03-18 13:43:52 +0100

    trying to figure out why results are different

--- src/models/classifier.py
+++ src/models/classifier.py
@@ -12,3 +12,16 @@ def mlp_classifier(latent_dim):
         layers.Dropout(0.3),
         layers.Dense(2, activation='softmax', kernel_regularizer=tf.keras.regularizers.l2(1e-4))
     ])
+
+def complex_mlp_classifier(latent_dim): 
+    return Sequential([
+        layers.Input(shape=(latent_dim,)),
+        layers.BatchNormalization(),
+        layers.Dense(32, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(1e-4)),
+        layers.Dropout(0.3),
+        layers.Dense(16, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(1e-4)),
+        layers.Dropout(0.3),
+        layers.Dense(8, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(1e-4)),
+        layers.Dropout(0.3),
+        layers.Dense(2, activation='softmax', kernel_regularizer=tf.keras.regularizers.l2(1e-4))
+    ])
