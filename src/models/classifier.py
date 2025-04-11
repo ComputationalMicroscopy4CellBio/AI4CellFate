3b64033d9373e139ef19c716d35c645631515026
Inês Cunha <inescunha200@gmail.com>
2025-04-11 14:35:09 +0200

    deleting comments

--- src/models/classifier.py
+++ src/models/classifier.py
@@ -6,9 +6,6 @@ from tensorflow.keras.regularizers import l2
 def mlp_classifier(latent_dim):
     return Sequential([
         layers.Input(shape=(latent_dim,)),
-        # layers.Dense(32, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(1e-4)), #
-        # layers.Dropout(0.3),
-        # layers.Dense(16, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(1e-4)),
         layers.Dropout(0.3),
         layers.Dense(2, activation='softmax', kernel_regularizer=tf.keras.regularizers.l2(1e-4))
     ])
