abf91d3a3ae06ae385bfe515c81eec7000dcec80
Inês Cunha <inescunha200@gmail.com>
2025-09-03 15:07:56 +0200

    trying to do second gen data

--- src/ai4cellfate.py
+++ src/ai4cellfate.py
@@ -10,12 +10,19 @@ def load_data():
     # TODO: replace with data loader
 
     # Augmented data - FIRST FRAME ONLY
-    augmented_x_train = np.load('./data/final_split/augmented_x_train.npy')
-    augmented_y_train = np.load('./data/final_split/augmented_y_train.npy')
-    x_val = np.load('./data/final_split/x_val.npy')
-    y_val = np.load('./data/final_split/y_val.npy')
-    x_test = np.load('./data/final_split/x_test.npy')
-    y_test = np.load('./data/final_split/y_test.npy')
+    augmented_x_train = np.load('./data/second_generation/augmented_x_traintrain_second_gen.npy')
+    augmented_y_train = np.load('./data/second_generation/augmented_y_train_second_gen.npy')
+    x_val = np.load('./data/second_generation/x_val_second_gen.npy')
+    y_val = np.load('./data/second_generation/y_val_second_gen.npy')
+    x_test = np.load('./data/second_generation/x_test_second_gen.npy')
+    y_test = np.load('./data/second_generation/y_test_second_gen.npy')
+    
+    print(f"Augmented train set: {augmented_x_train.shape[0]} samples")
+    print(f"Augmented train labels: {augmented_y_train.shape[0]} samples")
+    print(f"Validation set: {x_val.shape[0]} samples")
+    print(f"Validation labels: {y_val.shape[0]} samples")
+    print(f"Test set: {x_test.shape[0]} samples")
+    print(f"Test labels: {y_test.shape[0]} samples")
     
     return augmented_x_train, x_val, x_test, augmented_y_train, y_val, y_test
 
@@ -58,7 +65,7 @@ def main():
     }
 
     # Create parameter-based folder name
-    folder_name = (f"s1_ep{config_autoencoder['epochs']}_lr{config_autoencoder['lambda_recon']}"
+    folder_name = (f"second_gen_s1_ep{config_autoencoder['epochs']}_lr{config_autoencoder['lambda_recon']}"
                    f"_la{config_autoencoder['lambda_adv']}_seed{config_autoencoder['seed']}"
                    f"_ldim{config_autoencoder['latent_dim']}_s2_lr{config_ai4cellfate['lambda_recon']}"
                    f"_la{config_ai4cellfate['lambda_adv']}_lc{config_ai4cellfate['lambda_cov']}"
